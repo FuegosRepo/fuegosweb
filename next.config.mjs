072a941e7baf103530786469d2e7da402a987531
FuegosRepo <fuegos@proton.com>
2026-04-15 17:01:42 -0300

    perf: pre-optimize images with sharp (avg -85% file size)

--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,7 @@ const nextConfig = {
     ignoreBuildErrors: false,
   },
   images: {
-    unoptimized: false,
+    unoptimized: true,
     domains: ['localhost', 'images.unsplash.com'],
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
